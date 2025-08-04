3d221163020ccd3bab3e168e504ba14aa4bd546c
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-08-04 16:40:20 +0530

    Project 02

---
+++ Project 02/AddNumbers/src/main/java/com/AddValues/AddServlet.java
@@ -0,0 +1,21 @@
+package com.AddValues;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet 
+{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int a=Integer.parseInt(req.getParameter("num1"));		
+		int b=Integer.parseInt(req.getParameter("num2"));
+		int c=a+b;
+		PrintWriter out = res.getWriter();
+		out.println("Sum of "+a+" & "+b+" is "+c);
+	}
+	
+}
