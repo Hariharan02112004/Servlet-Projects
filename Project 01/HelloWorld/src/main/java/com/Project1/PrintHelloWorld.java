b9a5d2453678efaec8d13ef10b77fac58ac19efe
Hariharan G <167274701+Hariharan02112004@users.noreply.github.com>
2025-08-04 16:24:45 +0530

    Project 01

---
+++ Project 01/HelloWorld/src/main/java/com/Project1/PrintHelloWorld.java
@@ -0,0 +1,17 @@
+package com.Project1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrintHelloWorld extends HttpServlet {
+	public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException
+	{
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<p>Hello World</p>");
+	}
+}
